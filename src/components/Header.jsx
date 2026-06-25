211870a443ee9ca8c21e5ad3607bfed8eddfd4fa
Nicola Nigido <nicolanigido@MacBook-Pro-di-Nicola.local>
2026-06-25 14:20:00 +0200

    Add elegant logo design with updated header layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,8 +24,13 @@ export default function Header() {
     <header className="header">
       <div className="header-container">
         <Link to="/" className="logo" onClick={closeMenu}>
-          <span className="logo-text">Riviera Gulf</span>
-          <span className="logo-subtitle">Property Management</span>
+          <div className="logo-icon">
+            <img src="/images/riviera-gulf-logo.svg" alt="Riviera Gulf" />
+          </div>
+          <div className="logo-text-group">
+            <span className="logo-text">Riviera Gulf</span>
+            <span className="logo-subtitle">Property Management</span>
+          </div>
         </Link>
 
         <button className="menu-toggle" onClick={toggleMenu}>
